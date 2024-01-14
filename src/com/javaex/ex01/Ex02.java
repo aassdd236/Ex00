68e94cf3b2497f3be363d13875f98672278cfc33
aassdd236 <aassdd236@naver.com>
2024-01-14 16:26:30 +0900

    ArrayList

---
+++ src/com/javaex/ex01/Ex02.java
@@ -0,0 +1,12 @@
+package com.javaex.ex01;
+
+public class Ex02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	//	List<> sArray=new ArrayList<>();
+		
+		
+	}
+
+}
