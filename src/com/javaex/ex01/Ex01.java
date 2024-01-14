1128df25d65d0b249774990fced855d7b7ca25c5
aassdd236 <aassdd236@naver.com>
2024-01-14 15:41:04 +0900

    substring 연습

---
+++ src/com/javaex/ex01/Ex01.java
@@ -0,0 +1,13 @@
+package com.javaex.ex01;
+
+public class Ex01 {
+
+	public static void main(String[] args) {
+		String greeting = "Hello Java";
+		greeting.charAt( 0 ); //H
+		//greeting.charAt( 10 ); //에러
+		greeting.substring( 1, 3 ); //el
+
+	}
+
+}
